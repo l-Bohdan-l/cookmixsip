4cd20fbbd22158accb954c594e012574176acbe5
Bohdan <bpapushoj@gmail.com>
2023-08-08 23:14:44 +0300

    facts styles

--- src/components/Facts/Facts.styled.js
+++ src/components/Facts/Facts.styled.js
@@ -1,13 +1,41 @@
 import styled from "styled-components";
 
 export const FactWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  /* display: flex; */
+  /* flex-direction: column;
+  align-items: center; */
   width: 100%;
-  height: 300px;
+  /* height: 300px; */
   background-color: #0b0f15;
   border-radius: 10px;
   box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.75);
   border: 1px solid #000;
+  margin-bottom: 20px;
+`;
+
+export const Title = styled.h2`
+  color: rgb(226, 226, 226);
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: -0.928px;
+  text-align: center;
+  margin-bottom: 15px;
+`;
+
+export const Img = styled.img`
+  height: 170px;
+  border-radius: 10px 0 10px 0;
+  float: left;
+  margin-right: 10px;
+`;
+
+export const TextWrapper = styled.div`
+  padding: 10px;
+  text-align: left;
+`;
+
+export const Text = styled.p`
+  color: rgb(226, 226, 226);
 `;
