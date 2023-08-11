62d94bce0a2467f16515462ccf9242c406cea782
Bohdan <bpapushoj@gmail.com>
2023-08-11 12:10:18 +0300

    cocktails page

--- src/pages/Cocktails/Cocktails.jsx
+++ src/pages/Cocktails/Cocktails.jsx
@@ -1,16 +1,54 @@
 import { TextField } from "@mui/material";
-import { SearchFieldStyled, SectionStyled } from "./Cocktails.styled";
+import {
+  Form,
+  InputWrapper,
+  LabelSearchStyled,
+  List,
+  SearchBtn,
+  SearchFieldStyled,
+  SectionStyled,
+} from "./Cocktails.styled";
+
+import list from "../../cocktailList.json";
+import { useState } from "react";
 
 const Cocktails = () => {
+  const [cocktails, setCocktails] = useState(list);
   return (
     <SectionStyled>
-      <h1>Cocktails</h1>
-      <form>
-        <label>
-          Name:
-          <SearchFieldStyled type="text" name="name" />
-        </label>
-      </form>
+      <Form>
+        <LabelSearchStyled htmlFor="search-cocktail">
+          Find your cocktail:
+        </LabelSearchStyled>
+        <InputWrapper>
+          <SearchFieldStyled
+            id="search-cocktail"
+            type="search"
+            name="name"
+            placeholder="-- Mohito --"
+          />
+          <SearchBtn type="button">Search</SearchBtn>
+        </InputWrapper>
+      </Form>
+      <List>
+        {cocktails.map((cocktail) => {
+          let ingredients = [];
+          for (let i = 1; i <= 15; i++) {
+            if (cocktail[`strIngredient${i}`] !== null) {
+              ingredients.push(cocktail[`strIngredient${i}`]);
+            }
+          }
+          return (
+            <li key={cocktail.idDrink}>
+              <h2>{cocktail.strDrink}</h2>
+              <p>{cocktail.strAlcoholic}</p>
+              {ingredients.map((ingredient, index) => (
+                <p key={index}>{ingredient}</p>
+              ))}
+            </li>
+          );
+        })}
+      </List>
     </SectionStyled>
   );
 };
--- src/pages/Cocktails/Cocktails.styled.js
+++ src/pages/Cocktails/Cocktails.styled.js
@@ -5,6 +5,7 @@ export const SectionStyled = styled.section`
   /* background-color: antiquewhite; */
   margin-bottom: 40px;
   padding-bottom: 20px;
+  padding-top: 20px;
 `;
 
 // export const SearchFieldStyled = styled(TextField)`
@@ -18,6 +19,64 @@ export const SectionStyled = styled.section`
 export const SearchFieldStyled = styled.input`
   border: none;
   border-bottom: 2px solid #ff723e;
-  background-color: #ffffff7a;
+  background-color: rgb(57 56 56 / 27%);
   width: 100%;
+
+  border-radius: 5px;
+  padding: 5px;
+  font-size: 17px;
+  color: #ff723e;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const LabelSearchStyled = styled.label`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* margin-bottom: 20px; */
+  color: #ff723e;
+  font-size: 20px;
+  font-weight: 500;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  /* gap: 15px; */
+  margin-top: 10px;
+`;
+
+export const SearchBtn = styled.button`
+  border: 1px solid transparent;
+  border-radius: 5px;
+  background-color: #ff723e;
+  color: #fff;
+  font-size: 17px;
+  padding: 5px 10px;
+  cursor: pointer;
+  transition: all 250ms ease-in-out;
+  margin-left: 15px;
+  &:hover {
+    background-color: #fff;
+    color: #ff723e;
+    border: 1px solid #ff723e;
+    /* margin-left: 13px; */
+  }
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  background-color: white;
 `;
